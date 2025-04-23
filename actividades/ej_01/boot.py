e3959c4e86c067ba31d4ebcf4ab9b6969b4967c8
valentin <vzaccari33@gmail.com>
2025-04-23 15:45:01 -0300

    Update boot.py

--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,22 @@
-# Configuracion inicial
+
+import esp
+esp.osdebug(None)     
+
+import gc
+gc.collect()          
+
+import network
+from time import sleep
+
+def connect_to(ssid: str, passwd: str) -> str:
+    """
+    Conecta el ESP32 a la red Wi-Fi indicada y devuelve la IP asignada.
+    """
+    sta = network.WLAN(network.STA_IF)
+    if not sta.active():
+        sta.active(True)
+    if not sta.isconnected():
+        sta.connect(ssid, passwd)
+        while not sta.isconnected():
+            sleep(0.05)
+    return sta.ifconfig()[0]
